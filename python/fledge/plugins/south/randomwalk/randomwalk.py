b50bdf55a9426d0e6f4a37aec050b31e539fa07e
ashish-jabble <ashish@dianomic.com>
2024-12-05 19:04:58 +0530

    last preserved value fixes
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- python/fledge/plugins/south/randomwalk/randomwalk.py
+++ python/fledge/plugins/south/randomwalk/randomwalk.py
@@ -144,11 +144,13 @@ def plugin_reconfigure(handle, new_config):
     _LOGGER.info("Old config for randomwalk plugin {} \n new config {}".format(handle, new_config))
     new_handle = copy.deepcopy(new_config)
     new_handle['lastValue'] = handle['lastValue']
-    max_value = int(handle['maxValue']['value'])
+    max_value = int(new_handle['maxValue']['value'])
     # The maximum value cannot be less than the minimum value.
     # Therefore, setting the minimum value equal to the maximum value is required.
     if max_value < int(new_handle['minValue']['value']):
         new_handle['minValue']['value'] = max_value
+    if not(new_handle['lastValue'] <= int(new_handle['minValue']['value']) <= max_value):
+        new_handle['lastValue'] = randint(int(new_handle['minValue']['value']), max_value)
     return new_handle
 
 
